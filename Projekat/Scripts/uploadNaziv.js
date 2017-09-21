cce627516d35a172ad51aca14713029ae07ed004
Student <Student@ADMINRG-CDNBDLD>
2017-09-21 11:18:05 +0200

    Upload naziv centriranje i paddinzi na kontrolama

--- Projekat/Scripts/uploadNaziv.js
+++ Projekat/Scripts/uploadNaziv.js
@@ -1,12 +1,14 @@
 ﻿$(document).ready(function () {
     document.getElementsByClassName('inputfile')[0].onchange = uploadOnChange;
-    console.log('dwadawdaw');
+    //console.log('dwadawdaw');
+
     function uploadOnChange() {
         var filename = this.value;
         var lastIndex = filename.lastIndexOf("\\");
+
         if (lastIndex >= 0) {
             filename = filename.substring(lastIndex + 1);
         }
-        document.getElementById('filename').innerHTML = filename;
+        document.getElementById('filename').innerHTML = '<span><img src="../Content/img/upload2.png" alt="Izaberite Dokument">' + filename + '</span>';
     }
 });
