5ce26e625ba6f1e17944176dc7d9d41bedb72f46
Mladen <mladen@test.com>
2017-09-21 11:19:36 +0200

    Dodat modal za opis predmeta i skripta za get naziva i opisa. Dodat je i session storage za animaciju brisanja.

--- Projekat/Controllers/MaterijalController.cs
+++ Projekat/Controllers/MaterijalController.cs
@@ -141,7 +141,6 @@ namespace Projekat.Controllers
               
             }
             
-
             return RedirectToAction("MaterijaliPrikaz");
         }
     }
---
+++ Projekat/Scripts/modalOpisPredmet.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    console.log('Skripta radi');
+        
+    $('.opis').click(function () {
+        $opis = $(this);
+        var predmetNaziv = $opis.parent().parent().find('a:first').text();
+
+        var predmetOpis = $opis.parent().parent().find("div.opisPredmeta p").text();
+
+        console.log(predmetNaziv);
+        console.log(predmetOpis);
+
+        $('.modal-header span').text(predmetNaziv);
+        $('.modal-body p').text(predmetOpis);
+    });
+});
