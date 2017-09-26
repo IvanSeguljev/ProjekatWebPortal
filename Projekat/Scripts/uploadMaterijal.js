efe74c7e65d0f9738bce43f73730b8379961b93c
Mladen <mladen@test.com>
2017-09-26 11:27:47 +0200

    Skripta za upload materijala (kantica i validacija)

---
+++ Projekat/Scripts/uploadMaterijal.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    var data = sessionStorage.getItem('upload');
+    if (data) {
+        $('#snackbar').css('display', 'block');
+        sessionStorage.removeItem('kantica');
+    }
+
+    $('#postavi').click(function () {
+        sessionStorage.setItem('upload', true);
+        location.reload();
+    });
+
+
+    $('#postavkaMat').validate({
+        rules: {
+
+        },
+        messages: {
+
+        }
+    });
+
+});
