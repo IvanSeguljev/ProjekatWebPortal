33d0d43bacf31f87cd48425327657f328804d60f
Mladen <mladen@test.com>
2017-09-28 13:52:09 +0200

    Dodata skripta za filterMaterijala

---
+++ Projekat/Scripts/filterMaterijal.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    console.log('skripta radi');
+
+    $('.select2materijali').on('change', function () {
+        if(true){
+            $.ajax({
+                method: 'POST',
+                url: 'Materijal/MaterijaliPrikaz',
+                data: {
+
+                }
+
+            });
+        }
+    });
+
+
+});
