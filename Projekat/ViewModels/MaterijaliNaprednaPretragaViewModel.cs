16a88fdf103a4d010255998ca7651d020bd27066
dragan_kuzman <dragan@kuzman.com>
2017-11-03 10:34:24 +0100

    dodavanjeHelperaZaDropDownOptGroup

--- Projekat/ViewModels/MaterijaliNaprednaPretragaViewModel.cs
+++ Projekat/ViewModels/MaterijaliNaprednaPretragaViewModel.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Projekat.Models;
 
 namespace Projekat.ViewModels
 {
     public class MaterijaliNaprednaPretragaViewModel
     {
         public  IEnumerable<Projekat.Models.MaterijalModel> materijali { get; set; }
+
+        public MaterijalModel materijal { get; set; }
 
         public string naprednaPretragaSelektovani { get; set; }
     }
