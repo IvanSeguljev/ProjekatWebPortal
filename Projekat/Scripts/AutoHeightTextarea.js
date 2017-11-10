1ec8c9717c59efd1dadeacf1799c889998ab5179
Student <Student@ADMINRG-CDNBDLD>
2017-11-10 14:15:38 +0100

    Sredjeni modali za edit i opis smera i predmeta

---
+++ Projekat/Scripts/AutoHeightTextarea.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    
+    function textAreaAdjust(o) {
+        o.style.height = "1px";
+        o.style.height = (25 + o.scrollHeight) + "px";
+    }
+    //$('#editModal #predmetOpis').on("load keyup", function () {
+    $('#predmetOpis').keyup(function () {
+        textAreaAdjust(this);
+
+    });
+
+
+    $('#editModal').on('shown.bs.modal', function (e) {
+        var VisinaScrollaTexta = document.getElementById("predmetOpis").scrollHeight;
+        //$('#predmetOpis').style.height = "1px";
+        $('#predmetOpis').css("height", "1px");
+        $('#predmetOpis').css("height", 25 + VisinaScrollaTexta + "px");
+        //$('#predmetOpis').style.height = (25 + o.scrollHeight) + "px";
+        console.log(VisinaScrollaTexta);
+
+    })
+
+});
+
---
+++ Projekat/Scripts/editSmera.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    $('#editModal').on('shown.bs.modal', function () {
+        $('input, textarea').each(function () {
+
+            if ($(this).val().length !== 0) {
+                $(this).prev().filter(".matDesignLabel").addClass("fokusiraniMatDesignLabel");
+            }
+            else { }
+        });
+
+
+        $('input, textarea').on("keyup focus", function () {
+
+            if ($(this).val().length === 0) {
+                $(this).prev().filter(".matDesignLabel").removeClass("fokusiraniMatDesignLabel");
+                $(this).prev().filter(".matDesignLabel").css("color", "#6d6d6d");
+            }
+            else {
+                $(this).prev().filter(".matDesignLabel").addClass("fokusiraniMatDesignLabel");
+                $(this).prev().filter(".matDesignLabel").css("color", "#e21b22");
+            }
+        });
+
+        $('input, textarea').on("blur", function () {
+            if ($(this).val().length === 0) {
+                $(this).prev().filter(".matDesignLabel").removeClass("fokusiraniMatDesignLabel");
+
+            }
+            else {
+                $(this).prev().filter(".matDesignLabel").css("color", "#6d6d6d");
+            }
+        });
+    })
+
+
+});
