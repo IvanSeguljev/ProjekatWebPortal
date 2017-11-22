defe128c747cbcb2efcb7e550f6d39b3820e1561
dragan_kuzman <dragan@kuzman.com>
2017-11-22 14:09:55 +0100

    Predmeti Edit Back

---
+++ Projekat/Models/NamenaMaterijalaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace Projekat.Models
+{
+    public class NamenaMaterijalaModel
+    {
+        [Key]
+        public int namenaMaterijalaId { get; set; }
+
+        public string namenaMaterijalaNaziv { get; set; }
+
+    }
+}
