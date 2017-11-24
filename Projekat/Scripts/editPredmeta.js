5f999736de9b32194c9407550c31173073829db9
Mladen <mladen@test.com>
2017-11-24 13:00:59 +0100

    Fixovana skripta za filtriranje materijala i css

---
+++ Projekat/Scripts/editPredmeta.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $('.select2predmeti').select2();
+
+    var predmetEdited = sessionStorage.getItem('predmetEdited');
+
+    if (predmetEdited) {
+        $('#snackbar').css('display', 'block');
+        sessionStorage.removeItem('predmetEdited');
+    }
+    else  $('#snackbar').css('display', 'none');
+
+    var url = window.location.href;
+    var args = url.split('/');
+
+    var smerId = args[args.length - 1];
+
+    $('#submitEdit').click(function () {
+        $.ajax({
+            method: 'GET',
+            url: '/Predmet/VratiSmerove',
+            data: { smerId: smerId },
+            success: function () {
+                sessionStorage.setItem('predmetEdited', true);
+                location.reload();
+            }
+        });
+    });
+});
