32225c4a7799a49dafe6b69ce0cbe53a284e720d
dragan_kuzman <dragan@kuzman.com>
2018-01-10 15:35:02 +0100

    Revert "Edit Predmeta DoneRealy"
    This reverts commit f3a91dc018714d433cc22a1c3dda57b9d51f4f5f.

--- Projekat/Scripts/editPredmeta.js
+++ Projekat/Scripts/editPredmeta.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
 
-    $('.select2smerovi').select2();
+    $('.select2predmeti').select2();
 
     var isEdited = sessionStorage.getItem('editedPredmet');
 
@@ -11,15 +11,56 @@
     else
         $('#snackbar').css('display', 'none');
 
+    var url = window.location.href;
+    var args = url.split('/');
+
+    var smerId = args[args.length - 1];
+
+    var smerIds = [];
+
     $('.edit').click(function () {
         $edit = $(this);
         var predmetId = $edit.parent().parent().attr('id');
         var predmetNaziv = $edit.parent().parent().find("a[class='naziv-predmeta-na-kartici']").text();
         var predmetOpis = $edit.parent().parent().find("div.opisPredmeta p").text();
 
+        $edit = $(this);
+
+        $.ajax({
+            method: 'GET',
+            url: '/Predmet/VratiSmerove',
+            data: {
+                predmetId: predmetId
+            },
+            success: function (data) {
+                for (var smerId in data) {
+                    smerIds.push(smerId);
+                }
+                console.log(smerids);
+            }
+        });
+
         $('.modal-edit .modal-header span').text(predmetNaziv);
         $(".modal-edit .modal-body input[name='predmetId']").val(predmetId);
-        $(".modal-edit .modal-body input[name='predmetNaziv']").val(predmetNaziv);
-        $(".modal-edit .modal-body textarea[name='predmetOpis']").val(predmetOpis);
+        $(".modal-edit .modal-body input[name='predmet.predmetNaziv']").val(predmetNaziv);
+        $(".modal-edit .modal-body textarea[name='predmet.predmetOpis']").val(predmetOpis);
+    });
+
+    $('#submitEdit').click(function () {
+        $.ajax({
+            method: 'POST',
+            url: '/Predmet/Edit',
+            data: {
+                predmetNaziv: predmetNaziv,
+                predmetOpis: predmetOpis,
+                smeroviId: smerids,
+            },
+            complete: function () {
+                sessionStorage.setItem('editedPredmet', true);
+                smerids = [];
+                location.reload();
+            }
+        });
+    });
     });
 });
