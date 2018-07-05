cfcf16a942a104dbdad9d250590e325ac349dc7f
aleksandar <aleksandar29915@its.edu.rs>
2018-07-05 12:40:17 +0200

    Pomeren buton na desno

--- Projekat/Scripts/Korisnik/listaKorisnika.js
+++ Projekat/Scripts/Korisnik/listaKorisnika.js
@@ -3,11 +3,11 @@
 });
 $(document).ready(function () {
     var filter = '<button id="Filter" class="Filter" data-toggle="modal" data-target="#myModal">Napredna Pretraga</button>';
-    document.getElementById("listaKorisnika_filter").insertAdjacentHTML('afterbegin', filter);
     document.getElementById("listaKorisnika_filter").getElementsByTagName("label")[0].setAttribute("id", "LabelaPretrazi");
     var labela = document.getElementById("listaKorisnika_filter").getElementsByTagName("label")[0];
     var input = document.getElementById("listaKorisnika_filter").getElementsByTagName("label")[0].getElementsByTagName("input")[0];
     var div = document.getElementById("listaKorisnika_filter");
     labela.removeChild(input);
     div.insertAdjacentElement("beforeend", input);
+    document.getElementById("listaKorisnika_filter").insertAdjacentHTML('beforeend', filter);
 });
