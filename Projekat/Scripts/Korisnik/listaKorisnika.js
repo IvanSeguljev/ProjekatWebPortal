0c809e07e512f7351549a15c2632e1e2a84a73dc
Student <Student@ADMINRG-CDNBDLD>
2018-07-06 13:15:17 +0200

    sredjena labela

--- Projekat/Scripts/Korisnik/listaKorisnika.js
+++ Projekat/Scripts/Korisnik/listaKorisnika.js
@@ -3,7 +3,7 @@
 });
 $(function () {
     var filter = '<button id="Filter" class="Filter" data-toggle="modal" data-target="#myModal">NAPREDNA PRETRAGA</button>';
-    document.getElementById("listaKorisnika_filter").getElementsByTagName("label")[0].setAttribute("id", "LabelaPretrazi");
+    document.getElementById("listaKorisnika_filter").getElementsByTagName("label")[0].setAttribute("class", "matDesignLabel");
     var labela = document.getElementById("listaKorisnika_filter").getElementsByTagName("label")[0];
     var input = document.getElementById("listaKorisnika_filter").getElementsByTagName("label")[0].getElementsByTagName("input")[0];
     var div = document.getElementById("listaKorisnika_filter");
