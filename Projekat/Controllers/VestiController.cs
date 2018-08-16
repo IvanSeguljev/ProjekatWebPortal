c1276d4bdd281cad172dffcddf74ebb4f016ed25
aleksandar <aleksandar29915@its.edu.rs>
2018-08-16 13:22:25 +0200

    Dodavanje vesti stranica + editori

---
+++ Projekat/Controllers/VestiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projekat.Models;
+using Projekat.ViewModels;
+
+namespace Projekat.Controllers
+{
+    public class VestiController : Controller
+    {
+        [HttpGet]
+        public ActionResult NovaVest()
+        {
+            DodajVestViewModel vm = new DodajVestViewModel();
+            return View(vm);
+        }
+
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult SnimiVest(HttpPostedFileBase Fajl,DodajVestViewModel vm)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Projekat/ViewModels/DodajVestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projekat.ViewModels
+{
+    public class DodajVestViewModel
+    {
+        public string Naziv { get; set; }
+        public string Vest { get; set; }
+        
+    }
+}
