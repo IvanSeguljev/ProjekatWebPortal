653f2ac148f321c88115544a82acba8533bbc688
aleksandar <aleksandar29915@its.edu.rs>
2018-08-17 11:51:05 +0200

    vesti u bazi

---
+++ Projekat/Models/VestiContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Projekat.Models
+{
+    public class VestiContext:ApplicationDbContext
+    {
+        public DbSet<VestModel> Vesti { get; set; }
+    }
+}
