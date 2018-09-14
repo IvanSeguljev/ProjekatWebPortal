e4bc6e5b21ceffeb4b22393ad444b606f1d9b85b
aleksandar <aleksandar29915@its.edu.rs>
2018-09-14 13:17:18 +0200

    Dodavanje vesti validacija

--- Projekat/Scripts/Vesti/validacijaVesti.js
+++ Projekat/Scripts/Vesti/validacijaVesti.js
@@ -9,6 +9,11 @@
             KratakOpis: {
                 required: true,
                 opisReg: /^[A-za-z0-9\.\,\:\;\!\@\s\'\"\#\$\%\^\&\*\(\)\_\-\+\=\{\}\[\]\|\<\>\?\/]{0,150}$/
+            },
+            Fajl: {
+                required: true,
+                accept: "image/jpeg,image/png",
+                filesize: 2097152
             }
         },
         messages: {
@@ -17,10 +22,25 @@
             },
             KratakOpis: {
                 required: 'Ovo polje je obavezno'
+            },
+            Fajl: {
+               
+                accept: "Ekstenzija slike mora biti .jpg ili .png",
+                required: "Molimo vas odaberite sliku."
+            }
+
+        },
+        errorPlacement: function (error, element) {
+            if (element.attr("name") == "Fajl") {
+                error.insertAfter(element.next());
+            }
+            else {
+                error.insertAfter(element);
             }
 
         }
     });
+
     $.validator.addMethod("naslovReg", function (value, element, regexpr) {
         // allow any non-whitespace characters as the host part
         return regexpr.test(value);
@@ -30,6 +50,16 @@
 $.validator.addMethod("opisReg", function (value, element, regexpr) {
     // allow any non-whitespace characters as the host part
     return regexpr.test(value);
-    }, 'Polje moze sadrzati maksimum 150 karaktera');
+}, 'Polje moze sadrzati maksimum 150 karaktera');
+
+$.validator.addMethod('filesize', function (value, element, arg) {
+   
+    var minsize = 1000; // min 1kb
+    if ((element.files[0].size > minsize) && (element.files[0].size <= arg)) {
+        return true;
+    } else {
+        return false;
+    }
+},"Velicina slike ne sme biti veca od 2 MB");
 });
 
