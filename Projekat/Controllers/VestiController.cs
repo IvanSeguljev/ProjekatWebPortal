25b68443d012876595484c9a47d275e218a56811
aleksandar <aleksandar29915@its.edu.rs>
2018-10-12 11:49:59 +0200

    Autorizacija vesti :*

--- Projekat/Controllers/VestiController.cs
+++ Projekat/Controllers/VestiController.cs
@@ -90,6 +90,7 @@ namespace Projekat.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
+        [Authorize(Roles ="Administrator,Urednik")]
         public ActionResult NovaVest()
         {
             DodajVestViewModel vm = new DodajVestViewModel();
@@ -104,6 +105,7 @@ namespace Projekat.Controllers
         /// <returns></returns>
         [ValidateInput(false)]
         [HttpPost]
+        [Authorize(Roles = "Administrator,Urednik")]
         public ActionResult SnimiVest(HttpPostedFileBase Fajl, DodajVestViewModel vm)
         {
            
@@ -180,6 +182,7 @@ namespace Projekat.Controllers
         /// <param name="glavna">Da li je vest glavna vest, ako jeste, brise se i iz konfiguracije.</param>
         /// <returns></returns>
         [HttpPost]
+        [Authorize(Roles = "Administrator,Urednik")]
         public ActionResult BrisanjeVesti(int Id, bool glavna = false)
         {
            
@@ -247,6 +250,7 @@ namespace Projekat.Controllers
 
         [ValidateAntiForgeryToken]
         [HttpPost]
+        [Authorize(Roles = "Administrator,Urednik")]
         public ActionResult PostaviGlavnuVest(DateTime datum,string naslov,DateTime novDatum)
         {
             if(datum!= null && naslov != null && novDatum != null)
